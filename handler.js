264ab8ece6f9abe307879a36c518ede8b4920301
Vic-L <vljc17@gmail.com>
2019-04-17 14:36:55 -0500

    feat: core function to retrieve index.html page for non assets pages

--- handler.js
+++ handler.js
@@ -3,12 +3,26 @@
 // refer to https://aws.amazon.com/blogs/compute/implementing-default-directory-indexes-in-amazon-s3-backed-amazon-cloudfront-origins-using-lambdaedge/
 
 module.exports.removeExtension = async (event) => {
-  console.log(event)
+  let request = event.Records[0].cf.request
+  let uri = request.uri;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: JSON.stringify(event),
-    }),
-  };
+  console.log(`uri: ${uri}`);
+
+  // if is an asset request, just return request object
+  if (/\.[a-z]+$/.test(uri)) {
+    console.log('return request object unchanged');
+    return request;
+  }
+
+  // add trailing / if dont have
+  if (!/\/$/.test(uri)) {
+    uri += '/index.html';
+  } else {
+    uri += 'index.html'
+  }
+
+  request.uri = uri;
+
+  console.log(`return new request object with new uri: ${uri}`);
+  return request;
 };
