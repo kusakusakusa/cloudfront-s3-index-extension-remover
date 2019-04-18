659cd17dadd2b96b2512c37b769e64a6fe1df4fc
Vic-L <vljc17@gmail.com>
2019-04-17 20:36:17 -0500

    feat: addTrailingSlash function to handle uri without backslash not able to be considered as part of a cloudfront behaviour to be mutated by the removeExtension function in Origin request

--- handler.js
+++ handler.js
@@ -2,27 +2,63 @@
 
 // refer to https://aws.amazon.com/blogs/compute/implementing-default-directory-indexes-in-amazon-s3-backed-amazon-cloudfront-origins-using-lambdaedge/
 
+function isAssetRequest(uri) {
+  if (/\.[a-z]+$/.test(uri)) {
+    console.log('request is an asset request');
+    return true;
+  }
+
+  return false;
+}
+
+function hasTrailingSlash(uri) {
+  if (/\/$/.test(uri)) {
+    return true;
+  }
+
+  return false;
+}
+
 module.exports.removeExtension = async (event) => {
+  console.log(`event: ${JSON.stringify(event)}`);
   let request = event.Records[0].cf.request
   let uri = request.uri;
-
   console.log(`uri: ${uri}`);
 
   // if is an asset request, just return request object
-  if (/\.[a-z]+$/.test(uri)) {
-    console.log('return request object unchanged');
+  if (isAssetRequest(uri)) {
+    return request;
+  }
+
+  // add index.html to get to the root of the files
+  if (hasTrailingSlash(uri)) {
+    uri += 'index.html'
+  } else {
+    uri += '/index.html';
+  }
+
+  console.log(`return new request object with new uri: ${uri}`);
+  request.uri = uri;
+  return request;
+};
+
+module.exports.addTrailingSlash = async (event) => {
+  console.log(`event: ${JSON.stringify(event)}`);
+  let request = event.Records[0].cf.request
+  let uri = request.uri;
+  console.log(`uri: ${uri}`);
+
+  // if is an asset request, just return request object
+  if (isAssetRequest(uri)) {
     return request;
   }
 
   // add trailing / if dont have
-  if (!/\/$/.test(uri)) {
-    uri += '/index.html';
-  } else {
-    uri += 'index.html'
+  if (!hasTrailingSlash(uri)) {
+    uri += '/'
   }
 
-  request.uri = uri;
-
   console.log(`return new request object with new uri: ${uri}`);
+  request.uri = uri;
   return request;
 };
